fed2d1362580b51c34aa13e69e34634551b139eb
aniljarupula <sarithaanil38064@gmail.com>
2023-03-04 18:13:59 +0530

    v1

---
+++ day1/index.js
@@ -0,0 +1,15 @@
+function relation(){
+    if(document.getElementById("t1").value==document.getElementById("t2").value){
+        document.getElementById("res").innerHTML="both are equal";
+    }
+    else if(docmnet.getElementById("t2").value>document.getElementById("t3").value){
+        document.getElementById("res").innerHTML="both are equal";
+    }
+    else(document.getElementById("t3").value<document.getElementById("t1").value);{
+        document.getElementById("res").innerHTML="both are not equal";
+    }
+
+
+}
+
+
